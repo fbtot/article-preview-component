13b21c48bddcbe91131af9ee434d2c07f8bc18c2
Filippo Bristot <filippo.bristot@hotmail.it>
2021-10-20 17:29:19 +0200

    tooltip almost complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+let tooltipEl = document.getElementById("tooltipID");
+let shareIcon = document.getElementById("share-icon");
 
+shareIcon.addEventListener("click", (event) => {
+  tooltipEl.classList.toggle("clicked");
+});
